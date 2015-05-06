4f69adca84fa709ae74539a2087e30c18a68b444
UNA A. KRAVETS <uakravet@us.ibm.com>
2015-05-05 20:30:43 -0500

    fixed link to style :dog:

---
+++ app/templates/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/', { extensions: ['html'] }));
+app.use(express.static(__dirname + 'public'));
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
+});
+
+app.get('/about', function(req, res) {
+  res.sendFile('about.html');
+});
+
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+var port = (process.env.VCAP_APP_PORT || 5000);
+
+app.listen(port, host);
+console.log('App started on port ' + port);
